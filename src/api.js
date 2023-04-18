a9066111e9e0415a294d05812a673eec4477e5cd
easywarhead <filipe.freitas.s@hotmail.com>
2023-04-18 20:24:16 -0300

    removing node_modules

---
+++ src/api.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const router = express.Router();
+app.use(
+  "/",
+  router.get("/", (req, res, next) => {
+    res.status(200).send("<h1>API - CHAT</h1>");
+  })
+);
+
+app.use(
+  "/",
+  router.get("/about", (req, res, next) => {
+    res.status(200).send({
+      "name":"API - CHAT",
+      "version":"0.1.0",
+      "author":"Filipe Freitas"
+    });
+  })
+);
+
+module.exports = app;
